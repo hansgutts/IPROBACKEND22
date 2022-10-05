949729c859f9d7f6aa776b2de7d0e659652e2b4f
hansgutts <78183485+hansgutts@users.noreply.github.com>
2022-10-04 20:56:58 -0500

    Add files via upload
    The user creation. It checks to see if user exists as well.

---
+++ User_Creation.py
@@ -0,0 +1,36 @@
+from collections import UserList
+import mysql.connector
+import csv
+
+
+try:
+    userExist = ("SELECT EXISTS (SELECT Username FROM `backend`.`Users` WHERE Username=%s)")    #this query checks to see if this username exists (1 if yes 0 if no)
+    addUser = ("INSERT INTO backend.Users VALUES (%s, %s, %s)")
+    db = mysql.connector.connect(host='ipro-f22-db.crhoiczd7use.us-east-1.rds.amazonaws.com',   #log in to database
+                                  user='BackendAdmin',
+                                  password='R0ckBack3nd!',
+                                  database='backend')
+    print("connected")
+    
+    addUserCommand = db.cursor()
+    userExistsCommand = db.cursor()
+    with open('test.csv') as csvfile:   #open csv and reference as csvfile
+        reader = csv.reader(csvfile)
+        user = reader.__next__()        #read all data into one variable
+        csvfile.close()                 #close our connection to the file
+
+        userExistsCommand.execute(userExist, [user[0]])     #execute command check to see if the username is already taken
+        userList = userExistsCommand.fetchall()             #move this to a variable
+        if ((userList[0])[0] == 0) :                        #the 1 or 0 is given as [(1,)] or [(0,)]. if username does not exist  
+            addUserCommand.execute(addUser, user)           #create the user
+            print("User created")
+        else :
+            print("User exists. User cannot be created")    #otherwise print error message
+
+    db.commit()                                             #commit the changes
+    addUserCommand.close()                                  #close connections
+    userExistsCommand.close()
+    db.close()
+    print("Exited")
+except mysql.connector.Error as error:
+        print(error)
